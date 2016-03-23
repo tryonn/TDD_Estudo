6e8833e90521451c37efa10bd79d49a035914ba8
simao menezes <simao.menezes@grupogsh.com.br>
2016-03-23 13:45:52 -0300

    melhorando a class ConversorDeNumeroRomano

--- src/tdd/simao/ConversorDeNumeroRomano.java
+++ src/tdd/simao/ConversorDeNumeroRomano.java
@@ -5,21 +5,24 @@ import java.util.Map;
 
 public class ConversorDeNumeroRomano {
 	
-	private static Map<String, Integer> tabela = new HashMap<String, Integer>(){{
-		put("I", 1);
-		put("II", 2);
-		put("V", 5);
-		put("X", 10);
-		put("L", 50);
-		put("C", 100);
-		put("D", 500);
-		put("M", 1000);
+	private static Map<Character, Integer> tabela = new HashMap<Character, Integer>(){{
+		put('I', 1);
+		put('V', 5);
+		put('X', 10);
+		put('L', 50);
+		put('C', 100);
+		put('D', 500);
+		put('M', 1000);
 	}};
 	
 	
 	
 	
 	public int converte(String valor){
-		return tabela.get(valor);
+		int acumulador = 0;
+		for (int i = 0; i <valor.length(); i++) {
+			acumulador += tabela.get(valor.charAt(i)); 
+		}
+		return acumulador;
 	}
 }
