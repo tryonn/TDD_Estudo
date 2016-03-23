cc71ff011a1250c69ab30f685aeb3963f01a4b91
Simão Menezes <simaomenezesneto@gmail.com>
2016-03-23 16:00:10 -0300

    Refatorando a class CalculadoraDeSalarioTest

--- src/tdd/simao/calculoDeSalario/test/CalculadoraDeSalarioTest.java
+++ src/tdd/simao/calculoDeSalario/test/CalculadoraDeSalarioTest.java
@@ -17,7 +17,7 @@ public class CalculadoraDeSalarioTest {
 		
 		double salario = calculadora.calcular(funcionario);
 		
-		Assert.assertEquals(1500.0 * 0.9, salario, 0.00001);
+		Assert.assertEquals(funcionario.getSalario() * 0.9, salario, 0.00001);
 	}
 	
 	@Test
@@ -27,7 +27,7 @@ public class CalculadoraDeSalarioTest {
 		
 		double salario = calculadoraDeSalario.calcular(funcionario);
 		
-		Assert.assertEquals(4000.0 * 0.8, salario, 0.00001);
+		Assert.assertEquals(funcionario.getSalario() * 0.8, salario, 0.00001);
 	}
 	
 	@Test
@@ -36,7 +36,7 @@ public class CalculadoraDeSalarioTest {
 		Funcionario funcionario = new Funcionario("Luiz", 500.0, Cargo.DBA);
 		
 		double salario = calculadoraDeSalario.calcular(funcionario);
-		Assert.assertEquals(500 * 0.85, salario, 0.00001);
+		Assert.assertEquals(funcionario.getSalario() * 0.85, salario, 0.00001);
 	}
 
 }
